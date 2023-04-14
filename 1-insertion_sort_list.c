c649454cf82e595d9a0b41d0c88618c814612bbb
evanrich2404 <evanrich2404@gmail.com>
2023-04-13 22:36:02 -0500

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	locate = *list;
-	while(locate->next)
+	while (locate->next)
 	{
 		if (locate->n > locate->next->n)
 		{
